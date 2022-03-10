a48762542e48da4178c1f3a84b07c8072e190259
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2022-03-11 00:41:44 +0530

    quiz completed

--- quiz.js
+++ quiz.js
@@ -1,3 +1,23 @@
 const quizForm= document.querySelector(".quiz");
 const ansBtn= document.querySelector(".submit-btn");
-const quzRes= document.querySelector(".output")
+const quizRes= document.querySelector(".output");
+
+const correctAns= ["90°", "right-angeled", "equilateral"];
+
+function calculateScore(){
+    let score=0;
+    let index=0;
+    const formResults= new FormData(quizForm);  //formdata is an API used to access form results from a submission.
+    for(let entry of formResults.values()){
+        if (entry=== correctAns[index]){
+            score= score+1;
+        }
+        index=index+1;
+    }
+    quizRes.innerText= "Your score is: " + score;
+
+
+}
+
+
+ansBtn.addEventListener("click", calculateScore);
